4bd7bd173a35497774ef47e2ab16961d27c37dde
Franher <franher007@gmail.com>
2020-04-17 02:39:06 -0300

    Added DeleteBrowserControl

---
+++ DeleteBrowserControl.bat
@@ -0,0 +1,5 @@
+@echo off
+del C:\.jagex_cache_32\*.dll
+ren C:\.jagex_cache_32\browsercontrol.dll losercontrol%RANDOM%.dll
+del C:\Users\Franher\jagexcache\jagexlauncher\bin\.jagex_cache_32\*.dll
+ren C:\Users\Franher\jagexcache\jagexlauncher\bin\.jagex_cache_32\browsercontrol.dll losercontrol%RANDOM%.dll
