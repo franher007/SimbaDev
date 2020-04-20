ddc85f1a9f1599f34411be85169f3be217c3bf07
FRANCISCO HERRERA <francisco.herrera@mercadolibre.com>
2020-04-20 15:01:23 -0300

    Added new report system

--- DeleteBrowserControl.bat
+++ DeleteBrowserControl.bat
@@ -1,5 +1,5 @@
 @echo off
 del C:\.jagex_cache_32\*.dll
 ren C:\.jagex_cache_32\browsercontrol.dll losercontrol%RANDOM%.dll
-del C:\Users\Franher\jagexcache\jagexlauncher\bin\.jagex_cache_32\*.dll
-ren C:\Users\Franher\jagexcache\jagexlauncher\bin\.jagex_cache_32\browsercontrol.dll losercontrol%RANDOM%.dll
+del %UserProfile%\jagexcache\jagexlauncher\bin\.jagex_cache_32\*.dll
+ren %UserProfile%\jagexcache\jagexlauncher\bin\.jagex_cache_32\browsercontrol.dll losercontrol%RANDOM%.dll
